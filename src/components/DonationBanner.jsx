01d987a092f0f4fdf9266378ef388f9609e4d46b
mfm92 <mfmoser92@gmail.com>
2025-07-19 00:21:33 +0200

    actual white

--- src/components/DonationBanner.jsx
+++ src/components/DonationBanner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DonationBanner = () => (
-  <div className="w-full bg-gradient-to-r from-orange-700/75 via-green-700/75 to-black-100/75 py-4 px-6 flex justify-center items-center shadow-xl mb-8 rounded-xl">
+  <div className="w-full bg-gradient-to-r from-orange-700/75 via-green-700/75 to-white/75 py-4 px-6 flex justify-center items-center shadow-xl mb-8 rounded-xl">
     <span className="text-white text-lg font-semibold mr-4">
       🌟 Help make a difference! Support Canadian suicide prevention. Your
       donation can save lives.

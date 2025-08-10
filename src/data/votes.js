1d75d4d03a6fb83b198976eb2dcb358860f99c5a
mfm92 <mfmoser92@gmail.com>
2025-08-10 07:55:19 +0200

    ...

--- src/data/votes.js
+++
@@ -1,8 +0,0 @@
-// data/votes.js
-import { entries } from './entries.js';
-
-
-// these are fake btw :)
-export const votes = {
-  "BEG": { 12: "UGL", 10: "END", 8: "ZOM", 7: "ÖÖS", 6: "GRF", 5: "NAC", 4: "KON", 3: "NBS", 2: "XOC", 1: "WISN" },
-}
